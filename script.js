c6ddb9a6aa3f90b278ce95f560bac65e7278cba5
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-10-12 16:53:25 +0800

    add elements to DOM using javascript objects

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const myLibrary = [
     genre: "Philosophy",
     pages: "254",
     status: "reading"
-  }
+  },
   {
     name: "Atomic Habits", 
     author: "James Clear",
     genre: "Self Help",
     pages: "285",
     status: "not read"
-  }
+  },
   {
     name: "The Intelligent Investor", 
     author: "Benjamin Graham",
@@ -22,32 +22,43 @@ const myLibrary = [
   }
 ];
 
-// const tableHead = document.querySelector("tr")
-
-function Book(name, author, pages, description, genre) {
-  // the constructor...
-  this.name = name
-  this.author = author
-  this.pages = pages
-  this.description = description
-  this.genre = genre
-}
+// function Book(name, author, pages, description, genre) {
+//   this.name = name
+//   this.author = author
+//   this.pages = pages
+//   this.description = description
+//   this.genre = genre
+// }
 
 function addBookToLibrary() {
-  // do stuff here
+    let tableNumber = 1;
+
+    for (const book of myLibrary) {
+      const newRow = document.createElement("tr");
+      const bookName = document.createElement("td");
+      const bookAuthor = document.createElement("td");
+      const bookPages = document.createElement("td");
+      const bookGenre = document.createElement("td");
+      const bookStatus = document.createElement("td");
+      const tableBody = document.querySelector("tbody");
+
+      tableBody.appendChild(newRow).className = `table-row-${tableNumber}`;
+      newRow.appendChild(bookName).textContent = `${book.name}`;
+      newRow.appendChild(bookAuthor).textContent = `${book.author}`;
+      newRow.appendChild(bookGenre).textContent = `${book.genre}`;
+      newRow.appendChild(bookPages).textContent = `${book.pages}`;
+      newRow.appendChild(bookGenre).textContent = `${book.genre}`;
+      newRow.appendChild(bookStatus).textContent = `${book.status}`;
+      tableNumber++;
+    }
+    // for (const book of myLibrary) {
+    //   console.log("Name: " + book.name);
+    //   console.log("Author: " + book.author);
+    //   console.log("Genre: " + book.genre);
+    //   console.log("Pages: " + book.pages);
+    //   console.log("Status: " + book.status);
+    //   console.log("-----------");
+    // }
 }
 
-// function displayBooks() {
-//     const newRow = document.createElement("tr");
-//     const bookName = document.createElement("td");
-//     const bookAuthor = document.createElement("td");
-//     const bookPages = document.createElement("td");
-//     const bookDescription = document.createElement("td");
-//     const bookGenre = document.createElement("td");
-//     const bookStatus = document.createElement("td");
-// }
-
-// function bookStatus(name, status) {
-//   this.name = name  
-//   this.status = status
-// }
+addBookToLibrary();
