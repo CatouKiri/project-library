b6851cb0754328e485afee164b498648798c894e
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-10-22 18:06:57 +0800

    make the add button functional

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ---------- DEFAULT LIBRARY CONTENT ----------
 const myLibrary = [
   {
     name: "Meditations",
@@ -22,6 +23,7 @@ const myLibrary = [
   },
 ];
 
+// ---------- BOOK CONSTRUCTOR ----------
 function book(name, author, genre, pages, status) {
   this.name = name;
   this.author = author;
@@ -30,50 +32,57 @@ function book(name, author, genre, pages, status) {
   this.status = status;
 }
 
+// ---------- ADD BUTTON ----------
+let buttonBox = document.getElementById("add-button");
+buttonBox.onclick = inputValidation;
+
+// ---------- ADD NEWBOOK TO MYLIBRARY ----------
+function inputValidation(){
 let inputName = document.querySelector("#input-name").value;
-let inputAuthor = document.querySelector(".input-author");
-let inputGenre = document.querySelector(".input-genre");
-let inputPages = document.querySelector(".input-pages");
-let inputStatus = document.querySelector(".input-status");
+let inputAuthor = document.querySelector("#input-author").value;
+let inputGenre = document.querySelector("#input-genre").value;
+let inputPages = document.querySelector("#input-pages").value;
+let inputStatus = document.querySelector("#input-status").value;
 
-// let inputName = document.getElementById("input-name").value;
-// let inputAuthor = document.getElementById("input-author").value;
-// let inputGenre = document.getElementById("input-genre").value;
-// let inputPages = document.getElementById("input-pages").value;
-// let inputStatus = document.getElementById("input-status").value;
+addBookToLibrary(inputName, inputAuthor, inputGenre, inputPages, inputStatus);
+}
 
-document.getElementById("add-button").addEventListener("click", addBookToLibrary(inputName,inputAuthor,inputGenre,inputPages,inputStatus));
-
+// ---------- DISPLAY NEWBOOK ----------
 function addBookToLibrary(name, author, genre, pages, status) {
   let newBook = new book(name, author, genre, pages, status);
   myLibrary.push(newBook);
+  updateDisplay(newBook);
 }
 
-function displayBookToLibrary() {
-  let tableNumber = 1;
-  for (const book of myLibrary) {
-    const tableBody = document.querySelector("tbody");
-    const newRow = document.createElement("tr");
-    const bookNameColumn = document.createElement("td");
-    const bookAuthorColumn = document.createElement("td");
-    const bookPagesColumn = document.createElement("td");
-    const bookGenreColumn = document.createElement("td");
-    const bookStatusColumn = document.createElement("td");
-      bookStatusColumn.setAttribute('class','book-status-column');
-    const deleteColumn = document.createElement("td");
-      deleteColumn.setAttribute('class','delete-button-column');
-    const deleteButton = document.createElement("button");
-      deleteButton.setAttribute('class','delete-button');
+// ---------- DISPLAY BOOK ----------
+function updateDisplay(book){
+  const tableBody = document.querySelector("tbody");
+  const newRow = document.createElement("tr");
+  const bookNameColumn = document.createElement("td");
+  const bookAuthorColumn = document.createElement("td");
+  const bookPagesColumn = document.createElement("td");
+  const bookGenreColumn = document.createElement("td");
+  const bookStatusColumn = document.createElement("td");
+    bookStatusColumn.setAttribute('class','book-status-column');
+  const deleteColumn = document.createElement("td");
+    deleteColumn.setAttribute('class','delete-button-column');
+  const deleteButton = document.createElement("button");
+    deleteButton.setAttribute('class','delete-button');
 
-    tableBody.appendChild(newRow).className = `table-row-${tableNumber}`;
-    newRow.appendChild(bookNameColumn).textContent = `${book.name}`;
-    newRow.appendChild(bookAuthorColumn).textContent = `${book.author}`;
-    newRow.appendChild(bookGenreColumn).textContent = `${book.genre}`;
-    newRow.appendChild(bookPagesColumn).textContent = `${book.pages}`;
-    newRow.appendChild(bookStatusColumn).textContent = `${book.status}`;
-    newRow.appendChild(deleteColumn);
-    deleteColumn.appendChild(deleteButton).textContent = "Delete";
-    tableNumber++;
+  tableBody.appendChild(newRow).className = `table-row`;
+  newRow.appendChild(bookNameColumn).textContent = `${book.name}`;
+  newRow.appendChild(bookAuthorColumn).textContent = `${book.author}`;
+  newRow.appendChild(bookGenreColumn).textContent = `${book.genre}`;
+  newRow.appendChild(bookPagesColumn).textContent = `${book.pages}`;
+  newRow.appendChild(bookStatusColumn).textContent = `${book.status}`;
+  newRow.appendChild(deleteColumn);
+  deleteColumn.appendChild(deleteButton).textContent = "Delete";
+}
+
+// ---------- DISPLAY DEFAULT LIBRARY ----------
+function displayBookToLibrary() {
+  for (const book of myLibrary) {
+    updateDisplay(book);
   }
 }
 
